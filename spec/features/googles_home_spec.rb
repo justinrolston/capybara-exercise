b94f3bde6c10b8d4b9976822a43183471600fafe
Justin Rolston <justinrolston@gmail.com>
2014-10-18 14:11:09 -0400

    add travis

--- spec/features/googles_home_spec.rb
+++ spec/features/googles_home_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 feature 'Google Home Page' do
+  background do
+    visit '/'
+  end
 
   scenario 'Title Check' do
-    visit '/'
     expect(page).to have_title 'Google'
   end
 
   scenario 'Search', :js => 'true' do
-    visit '/'
     find('input[name=q]').set('therolstons.com')
     find('button#gbqfb.gbqfb').click
     expect(page).to have_content("The Rolston's: About Us www.therolstons.com")
   end
-  
+
 end
